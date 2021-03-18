eb9da2b2c1ee9c9e77f9bf7d2f918e9e8d80ba7d
Vadim Selivestrau <74891951+Vadim-Selivestrau@users.noreply.github.com>
2021-03-19 00:05:11 +0300

    Update Program.cs

--- lab2/task3/Program.cs
+++ lab2/task3/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 namespace task3
 {
     class Program
@@ -6,25 +6,25 @@ namespace task3
         static void Main(string[] args)
         {
             Console.WriteLine("input first number");
-            int FirstUserAnswer, SecondUserAnswer;
-            while (!int.TryParse(Console.ReadLine(), out FirstUserAnswer))
+            ulong FirstUserAnswer, SecondUserAnswer;
+            while (!ulong.TryParse(Console.ReadLine(), out FirstUserAnswer))
             {
                 Console.WriteLine("Error Input");
             }
             Console.WriteLine("input second number");
-            while (!int.TryParse(Console.ReadLine(), out SecondUserAnswer))
+            while (!ulong.TryParse(Console.ReadLine(), out SecondUserAnswer))
             {
                 Console.WriteLine("Error Input");
             }
             if (FirstUserAnswer > SecondUserAnswer)
             {
-                int temp = FirstUserAnswer;
+                ulong temp = FirstUserAnswer;
                 FirstUserAnswer = SecondUserAnswer;
                 SecondUserAnswer = temp;
             }
-            int tmp = SecondUserAnswer;
-            int kount = 0;
-            int number = tmp;
+            ulong tmp = SecondUserAnswer;
+            ulong kount = 0;
+            ulong number = tmp;
             while (number != 1)
             {
                 tmp = number;
