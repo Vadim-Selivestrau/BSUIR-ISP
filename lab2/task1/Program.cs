ac812c4890d2a7232d84adad588882b57d33d583
Vadim Selivestrau <74891951+Vadim-Selivestrau@users.noreply.github.com>
2021-03-18 22:15:08 +0300

    Add files via upload

---
+++ lab2/task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            string Alphabet = "qwertyuioplkjhgfdsazxcvbnm";
+            string Output = "";
+            int Length = rnd.Next(1, 4);
+            for (int i = 0; i < Length; i++)
+            {
+                int temp = rnd.Next(0, Alphabet.Length);
+                Output += Alphabet[temp];
+            }
+            Console.WriteLine($"Your random string is {Output}");
+        }
+    }
+}
