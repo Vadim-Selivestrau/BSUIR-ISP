648a5ec8ff1bb6fcaac385b9b15918ebb3bcf84d
Vadim Selivestrau <74891951+Vadim-Selivestrau@users.noreply.github.com>
2021-03-19 20:30:06 +0300

    Add files via upload

---
+++ lab2/task2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void revers(String str)
+        {
+            string[] Word = str.Split(' ');
+            for (int i = Word.Length - 1; i >= 0; i--)
+            {
+                Console.Write("{0} ", Word[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input your string");
+            string input = Console.ReadLine();
+            revers(input);
+        }
+    }
+}
