0cbaeadd5689e764173d0e07aa00f158505e4d40
Vadim Selivestrau <74891951+Vadim-Selivestrau@users.noreply.github.com>
2021-05-14 21:28:35 +0300

    Add files via upload

---
+++ laba4/lab4_2/MyLibrary/MyLibrary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public static class MyLibrary
+    {
+        public static int Factorial(int n)
+        {
+            unsafe
+            {
+                if (n == 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return n * Factorial(n - 1);
+                }
+            }
+        }
+        public static int Fibonachi(int n)
+        {
+            unsafe
+            {
+                if (n == 0 || n == 1)
+                {
+                    return n;
+                }
+                else
+                {
+                    return Fibonachi(n - 1) + Fibonachi(n - 2);
+                }
+            }
+        }
+    }
+}
