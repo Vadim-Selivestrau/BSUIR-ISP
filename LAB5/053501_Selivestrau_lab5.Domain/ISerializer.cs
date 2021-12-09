7f2a660eb63d52c6485e040a226ce3df677373ac
Vadim Selivestrau <selivestrau2003@gmail.com>
2021-12-09 13:28:59 +0300

    Add files via upload

---
+++ LAB5/053501_Selivestrau_lab5.Domain/ISerializer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _053501_Selivestrau_lab5.Domain
+{
+    public interface ISerializer
+    {
+        IEnumerable<Hospital> DeSerializeByLINQ(string fileName);
+        IEnumerable<Hospital> DeSerializeXML(string fileName);
+        IEnumerable<Hospital> DeSerializeJSON(string fileName);
+        void SerializeByLINQ(IEnumerable<Hospital> xxx, string fileName);
+        void SerializeXML(IEnumerable<Hospital> xxx, string fileName);
+        void SerializeJSON(IEnumerable<Hospital> xxx, string fileName);
+    }
+}
